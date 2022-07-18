f2818f211c5409f40e86e2acf0cb21adc4655d0c
Kishore RR <66819332+kishorerr@users.noreply.github.com>
2022-07-18 20:48:05 +0530

    Update railtie.rb

--- lib/communify/railtie.rb
+++ lib/communify/railtie.rb
@@ -7,7 +7,7 @@ module Communify
 		communify_generator_path = Communify::root.+'/communify/communify/communify_generator.rb'
 
 		config.app_generators do
-			require service_generator_path
+			require communify_generator_path
 		end
 	end
 end
