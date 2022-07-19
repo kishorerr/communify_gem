5c1e5ed76e0365d400f87a29c6cf4c40d7bb3959
Kishore RR <66819332+kishorerr@users.noreply.github.com>
2022-07-19 08:25:56 +0530

    Update sms_migration_generator.rb

--- lib/communify/sms/sms_migration_generator.rb
+++ lib/communify/sms/sms_migration_generator.rb
@@ -1,19 +1,19 @@
 require 'rails/generators'
 require 'fileutils'
 
-class SmsMigrationGenerator < Rails::Generators::NamedBase
+class SmsMigrationGenerator < Rails::Generators::Migration
   source_root File.expand_path('../templates', __FILE__)
 
   argument :methods, type: :array, default: [], banner: "method method"
   class_option :module, type: :string
 
-  def create_service_file
+  def create_sms_migration
     @module_name = options[:module]
 
     communify_dir_path = Rails.root.join 'db', 'migrate'    
     generator_path = communify_dir_path.join "#{file_name}.rb"
 
-    template "sms.erb", generator_path
+    migration_template "sms.erb", "#{communify_dir_path}/sms_#{table_name}.rb"
   end
   
   private
