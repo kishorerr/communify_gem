3538fd460edb3305495c27380aff3279912332c9
Kishore RR <66819332+kishorerr@users.noreply.github.com>
2022-07-19 11:16:37 +0530

    communify

--- lib/communify/sms/sms_migration_generator.rb
+++ lib/communify/sms/sms_migration_generator.rb
@@ -12,7 +12,7 @@ class SmsMigrationGenerator < ActiveRecord::Generators::Base
     communify_dir_path = Rails.root.join 'db', 'migrate'    
     generator_path = communify_dir_path.join "#{file_name}.rb"
 
-    migration_template "sms.erb", "#{communify_dir_path}/sms_#{table_name}.rb", migration_version: migration_version
+    migration_template "sms.erb", "#{communify_dir_path}/create_#{table_name}.rb", migration_version: migration_version
   end
   
   private
