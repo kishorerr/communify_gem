dbe9e982127db0c79aef2a8b9950878bd2f00444
Kishore RR <66819332+kishorerr@users.noreply.github.com>
2022-07-19 07:14:20 +0530

    Update sms_migration_generator.rb

--- lib/communify/generators/sms/sms_migration_generator.rb
+++ lib/communify/generators/sms/sms_migration_generator.rb
@@ -1,21 +1,27 @@
-class DataMigrationGenerator < Rails::Generators::NamedBase
-    def create_data_migration_file
-      timestamp = Time.zone.now.to_s.tr('^0-9', '')[0..13]
-      filepath = "db/migrate/#{timestamp}_#{file_name}.rb"
-  
-      create_file filepath, <<-FILE
-      # frozen_string_literal: true
-  
-      class #{class_name} < ActiveRecord::Migration[7.0]
-        def change
-            create_table :todos do |t|
-                t.string :title
-                t.string :created_by
-          
-                t.timestamps
+module Communify
+    module Generators
+        module Sms
+            class SmsMigrationGenerator < Rails::Generators::NamedBase
+                def create_data_migration_file
+                  timestamp = Time.zone.now.to_s.tr('^0-9', '')[0..13]
+                  filepath = "db/migrate/#{timestamp}_#{file_name}.rb"
+              
+                  create_file filepath, <<-FILE
+                  # frozen_string_literal: true
+              
+                  class #{class_name} < ActiveRecord::Migration[7.0]
+                    def change
+                        create_table :todos do |t|
+                            t.string :title
+                            t.string :created_by
+                      
+                            t.timestamps
+                          end
+                    end
+                  end
+                  FILE
+                end
               end
         end
-      end
-      FILE
     end
-  end
+end
