43aae64f286ee307fd60069fb0b0c64aaf5eac04
Kishore RR <66819332+kishorerr@users.noreply.github.com>
2022-07-20 10:20:03 +0530

    communify

--- lib/communify.rb
+++ lib/communify.rb
@@ -1,5 +1,5 @@
 require "communify/version"
-require 'communify/generators/sms/sms_railtie' if defined?(Rails)
+require 'communify/railtie' if defined?(Rails)
 
 class Error < StandardError; end
 
---
+++ lib/communify/railtie.rb
@@ -0,0 +1,16 @@
+module Communify
+	def self.root
+		File.dirname __dir__
+	end
+
+	class InstallRailtie < Rails::Railtie
+		sms_generator_path = Communify::root.+'/generators/sms/sms_migration_generator.rb'
+		install_generator_path = Communify::root.+'/generators/install/install_generator.rb'
+
+
+		config.app_generators do
+			require sms_generator_path
+			require install_generator_path
+		end
+	end
+end
