d8946d2c224277ec657fb622c298a00c8951270d
Kishore RR <66819332+kishorerr@users.noreply.github.com>
2022-07-20 22:12:36 +0530

    communify

--- lib/communify/controllers/sms_controller.rb
+++ lib/communify/controllers/sms_controller.rb
@@ -3,16 +3,14 @@ require 'communify'
 module Communify
     module Controllers
         class Sms 
-            # attr_accessor :account_sid, :auth_token
-
-            def self.send_message (message = "Hello")
+            def self.send_message (resource)
                 account_sid = Communify.account_sid
                 auth_token = Communify.auth_token
                 @client = Twilio::REST::Client.new account_sid, auth_token
                 @client.messages.create(
                     from: Communify.sender_no,
-                    to: '+917356610002',
-                    body: message
+                    to: resource.recipient_no,
+                    body: resource.message
                 )
             end
         end
