e77e1900741d7c1e5f34f199b54c86fa1f183ed1
Kishore RR <66819332+kishorerr@users.noreply.github.com>
2022-07-21 20:37:56 +0530

    Update sms_controller.rb

--- lib/communify/controllers/sms_controller.rb
+++ lib/communify/controllers/sms_controller.rb
@@ -6,8 +6,6 @@ module Communify
                 resource.save
                 puts resource.read_attribute_before_type_cast(:priority)
                     Communify::Workers::PriorityWorker.perform_async(resource.recipient_number, resource.message, resource.read_attribute_before_type_cast(:priority))
-                end
-                
             end
         end
     end
