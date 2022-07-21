f8f5b39fffb2af45565e05e731d08a04f0a4cfd4
Kishore RR <66819332+kishorerr@users.noreply.github.com>
2022-07-21 12:27:36 +0530

    Update sms_controller.rb

--- lib/communify/controllers/sms_controller.rb
+++ lib/communify/controllers/sms_controller.rb
@@ -8,7 +8,7 @@ module Communify
                 account_sid = Communify.account_sid
                 auth_token = Communify.auth_token
                 Thread.new do
-                    sleep(2.minutes)
+                    sleep(resource.priority.minutes)
                     @client = Twilio::REST::Client.new account_sid, auth_token
                     @client.messages.create(
                         from: Communify.sender_no,
