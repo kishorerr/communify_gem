ef4c398d7f2bf915e634426e6b4b8faf91f035b1
Kishore RR <66819332+kishorerr@users.noreply.github.com>
2022-07-22 14:24:55 +0530

    Update sms_controller.rb

--- lib/communify/controllers/sms_controller.rb
+++ lib/communify/controllers/sms_controller.rb
@@ -3,27 +3,21 @@ require 'sidekiq/api'
 module Communify
     module Controllers
         class Sms 
-            attr_accessor :resource
-            def initialize (resource)
-              @resource = resource
-            end
+            class << self
+                attr_accessor :resource
 
-            def self.resource
-                resource
-                puts resource
-            end
-
-            def self.send_message (resource)
-                Sms.new(resource)
-                if resource.save
-                    resource.update_column(:message_status, "Message Queued at #{DateTime.now}")
-                    time = resource.read_attribute_before_type_cast(:priority)
-                    result = Communify::Workers::PriorityWorker.perform_in(time.minutes.from_now,resource.recipient_number, resource.message)
-                    return result
-                else    
-                    raise "Error => Resource has not been saved!!"
+                def send_message (resource)
+                    Sms.new(resource)
+                    if resource.save
+                        resource.update_column(:message_status, "Message Queued at #{DateTime.now}")
+                        time = resource.read_attribute_before_type_cast(:priority)
+                        result = Communify::Workers::PriorityWorker.perform_in(time.minutes.from_now,resource.recipient_number, resource.message)
+                        return result
+                    else    
+                        raise "Error => Resource has not been saved!!"
+                    end
                 end
-            end
+            end            
         end
     end
 end
