65e1d5c2d269cf4be19f1e34c7673f6edde45a1b
Kishore RR <66819332+kishorerr@users.noreply.github.com>
2022-07-22 11:00:24 +0530

    Update sms_controller.rb

--- lib/communify/controllers/sms_controller.rb
+++ lib/communify/controllers/sms_controller.rb
@@ -8,11 +8,6 @@ module Communify
                     resource.update_column(:message_status, "Message Queued at #{DateTime.now}")
                     time = resource.read_attribute_before_type_cast(:priority)
                     result = Communify::Workers::PriorityWorker.perform_in(time.minutes.from_now,resource.recipient_number, resource.message)
-                    job = Sidekiq::RetrySet.new.find_job(result)
-                    if result != 'null'
-                        resource.update_column(:message_status, "Message Delivered at #{DateTime.now}")
-                    end
-                    return job
                 else    
                     raise "Error => Resource has not been saved!!"
                 end
