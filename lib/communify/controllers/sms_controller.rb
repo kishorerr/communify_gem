9b01fb6dfc049b9525c78612a8f73d7581c85343
Kishore RR <66819332+kishorerr@users.noreply.github.com>
2022-07-22 14:11:45 +0530

    Update sms_controller.rb

--- lib/communify/controllers/sms_controller.rb
+++ lib/communify/controllers/sms_controller.rb
@@ -4,10 +4,11 @@ module Communify
     module Controllers
         class Sms 
             attr_accessor :resource
-            def def initialize(resource)
+            def initialize (resource)
               @resource = resource
             end
-            def self.send_message(resource)
+
+            def self.send_message (resource)
                 Sms.new(resource)
                 if resource.save
                     resource.update_column(:message_status, "Message Queued at #{DateTime.now}")
