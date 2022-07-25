dbeccfa0616b213195a8babed629c6be4bdb8d6c
Kishore RR <66819332+kishorerr@users.noreply.github.com>
2022-07-25 20:06:25 +0530

    Update priority_worker.rb

--- lib/communify/workers/priority_worker.rb
+++ lib/communify/workers/priority_worker.rb
@@ -6,7 +6,12 @@ module Communify
     module Workers 
         class PriorityWorker
             include Sidekiq::Worker
-            sidekiq_options retry: 0, dead: false
+            sidekiq_options retry: 3, dead: false
+            sidekiq_retries_exhausted do |job, e|
+                resource_id = job['args'].first
+                @failed_resource = CommunifySms.find(resource_id)
+                @failed_resource.update_column(:message_status, "Message Failed at #{DateTime.now} due to error => #{e}")
+            end
 
             def perform(recipient_number, message, resource_id, time, attempt)
                 puts attempt
@@ -18,16 +23,12 @@ module Communify
                 # puts @current_resource.id
                 begin
                     @client.messages.create(from: Communify.sender_no,to: recipient_number,body: message) 
-                    @current_resource.update_column(:attempt_count, attempt)
-                    @current_resource.update_column(:message_status, "Message Delivered at #{DateTime.now}")
+                    
                 rescue Twilio::REST::RestError => e
-                    if attempt < 4
-                        attempt = attempt + 1
-                        Communify::Workers::PriorityWorker.perform_in(time.minutes.from_now, @current_resource.recipient_number, @current_resource.message, @current_resource.id, time, attempt)
-                    else
-                        @current_resource.update_column(:message_status, "Message Failed at #{DateTime.now} due to error => #{e}")
-                    end
+                   raise e
                 end
+                @current_resource.update_column(:attempt_count, attempt)
+                @current_resource.update_column(:message_status, "Message Delivered at #{DateTime.now}")
             end
         end
     end
