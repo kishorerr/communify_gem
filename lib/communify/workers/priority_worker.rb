37815fc4bdca92e5feb88e6b7dc331115f669ca9
Kishore RR <66819332+kishorerr@users.noreply.github.com>
2022-07-26 07:39:10 +0530

    Update priority_worker.rb

--- lib/communify/workers/priority_worker.rb
+++ lib/communify/workers/priority_worker.rb
@@ -6,7 +6,7 @@ module Communify
     module Workers 
         class PriorityWorker
             include Sidekiq::Worker
-            sidekiq_options dead: false
+            sidekiq_options dead: false, retry: false
 
             def perform(resource_id, recipient_number, message, time, attempt)
                 account_sid = Communify.account_sid
@@ -15,8 +15,7 @@ module Communify
                 @current_resource = CommunifySms.find(resource_id)
                 begin
                     twilio_response = @client.messages.create(from: Communify.sender_no,to: recipient_number,body: message)
-                rescue Twilio::REST::RestError => e
-                    puts "ello #{topper}"
+                rescue Exception => e
                     @current_resource.update_column(:attempt_count, attempt)
                     attempt = attempt +1
                     @current_resource.update_column(:message_status, "Message Failed at #{DateTime.now} due to error => #{e}")
